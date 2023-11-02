321c89cdb9281bad050891fa6750c6daff1e4874
Daniel Power <me@danielpower.ca>
2023-11-01 23:15:53 -0230

    Prettier

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,7 @@
-import { DATABASE_URL } from '$env/static/private';
-import DatabaseConstructor from 'better-sqlite3';
-import type { Database } from 'better-sqlite3';
+import { DATABASE_URL } from "$env/static/private";
+import DatabaseConstructor from "better-sqlite3";
+import type { Database } from "better-sqlite3";
 
-export const db: Database = new DatabaseConstructor(DATABASE_URL.replace('sqlite:', ''));
+export const db: Database = new DatabaseConstructor(
+	DATABASE_URL.replace("sqlite:", ""),
+);
