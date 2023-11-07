d7cb00ddd66585d22b5d5b219ccd10241545c3bd
Daniel Power <me@danielpower.ca>
2023-11-07 19:29:37 -0330

    Add trivial menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,47 +2,67 @@
 	import { page } from "$app/stores";
 	import Fa from "svelte-fa";
 	import { faHome, faQuestionCircle } from "@fortawesome/free-solid-svg-icons";
+	import Sidebar from "./Sidebar.svelte";
 </script>
 
 <div class="container">
-	<a href="/">
-		<Fa icon={faHome} size="1.5x" />
-	</a>
-	<div class="nav reverse">
-		{#if $page.data.leftNavComponent}
-			<svelte:component this={$page.data.leftNavComponent} />
-		{/if}
+	<div class="topbar">
+		<a href="/">
+			<Fa icon={faHome} size="1.5x" />
+		</a>
+		<div class="nav reverse">
+			{#if $page.data.leftNavComponent}
+				<svelte:component this={$page.data.leftNavComponent} />
+			{/if}
+		</div>
+		<div class="title">{$page.data.title}</div>
+		<div class="nav">
+			{#if $page.data.rightNavComponent}
+				<svelte:component this={$page.data.rightNavComponent} />
+			{/if}
+		</div>
+		<a href="/about">
+			<Fa icon={faQuestionCircle} size="1.5x" />
+		</a>
 	</div>
-	<div class="title">{$page.data.title}</div>
-	<div class="nav">
-		{#if $page.data.rightNavComponent}
-			<svelte:component this={$page.data.rightNavComponent} />
-		{/if}
+	<div class="sidebar">
+		<Sidebar />
 	</div>
-	<a href="/about">
-		<Fa icon={faQuestionCircle} size="1.5x" />
-	</a>
-</div>
-<div class="content">
-	<slot />
+	<div class="content">
+		<slot />
+	</div>
 </div>
 
 <style>
+	.container {
+		display: grid;
+		grid-template-columns: fit-content(1rem) 1fr;
+		grid-template-rows: fit-content(1rem) 1fr;
+		grid-template-areas:
+			"topbar topbar"
+			"sidebar content";
+		min-height: 100vh;
+	}
 	.title {
 		text-align: center;
 		font-size: 1.1rem;
 	}
-	.container {
+	.topbar {
 		display: flex;
 		gap: 1rem;
+		grid-area: topbar;
 		background: burlywood;
 		padding: 1rem;
 	}
-	.container a {
+	.topbar a {
 		color: black;
 	}
-	.container a:hover {
+	.topbar a:hover {
 		color: white;
+	}
+	.sidebar {
+		grid-area: sidebar;
+		max-width: 10rem;
 	}
 	.content {
 		padding: 1rem;
---
+++ src/routes/Sidebar.svelte
@@ -0,0 +1,24 @@
+<nav>
+	<a href="/">Today</a>
+	<a href="/agenda">Agenda</a>
+</nav>
+
+<style>
+	nav {
+		background: wheat;
+		padding: 1rem;
+		height: 100%;
+	}
+	nav {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+	}
+	nav a {
+		color: black;
+		text-decoration: none;
+	}
+	nav ul li a:hover {
+		color: white;
+	}
+</style>
