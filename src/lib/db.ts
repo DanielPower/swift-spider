a4d310f3a4a40f08b9321e7ba44215ebbdfdd56d
Daniel Power <me@danielpower.ca>
2023-11-08 01:54:44 -0330

    Fix database env reading

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,9 @@
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import Database from "better-sqlite3";
 import { migrate } from "drizzle-orm/better-sqlite3/migrator";
-import { DATABASE_PATH } from "$env/static/private";
+import { env } from "$env/dynamic/private";
 
-const sqlite = new Database(`${DATABASE_PATH}/db.sqlite3`);
+const sqlite = new Database(`${env.DATABASE_PATH}/db.sqlite3`);
 export const db = drizzle(sqlite);
 
 migrate(db, { migrationsFolder: "drizzle" });
